4faef5d3783e9ce808ee952d16d822769844f9f9
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-19 17:06:16 -0500

    hmmm

--- 99lines.js
+++ 99lines.js
@@ -1,5 +1,28 @@
+
+
+
+
 var friends = ['Bean ', 'Pierre ', 'Miesha ', 'Brandon ', 'Rayshawn '];
+// for (x=0;x<friends.length; x++){
+//     for (i=99; i > 0; i--) {
+
+//         console.log([++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
+    
+//     }
+//     console.log([++i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' + ' no more lines of code on the wall');
+// }
+
+
+// var button = documentCreateElement('button');
+// document.body.appendChild(button);
+// button.addEventListener('click',singSong)
+function singSong(){
 for (x=0;x<friends.length; x++){
+     var div = documentCreateElement('div');
+    div.className = 'div1';
+    var h3 = documentCreateElement('h3');
+    div1.appendChild(h3);
+    document.body.appendChild(div1);
     for (i=99; i > 0; i--) {
 
         console.log([++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
@@ -7,5 +30,7 @@ for (x=0;x<friends.length; x++){
     }
     console.log([++i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' + ' no more lines of code on the wall');
 }
+}
+
 
 
