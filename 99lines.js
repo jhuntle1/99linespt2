e7aa5e77eb01790eaf1a4355cc2201e86ff87cc1
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-19 10:14:47 -0500

    first commit

---
+++ 99lines.js
@@ -0,0 +1,38 @@
+// var friends = ['Bean ', 'Pierre ', 'Miesha ', 'Brandon ', 'Rayshawn '];
+// for (x=0;x<friends.length; x++){
+//     for (i=99; i > 1; i--) {
+
+//         console.log([++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
+    
+//     }
+//     console.log([i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' +[i] + ' line of code on the wall');
+// }
+
+
+
+// ------------------
+
+
+
+// var text = ""
+// var i = 0;
+
+// do {
+//     console.log(text += "The number is " + [+i]);
+//     i++;
+// }
+// while (i < 100);  
+
+
+
+
+var friends = ['Bean ', 'Pierre ', 'Miesha ', 'Brandon ', 'Rayshawn '];
+var i = 99; i>1, i--
+var x = 0; x<friends.length; x++
+
+do {
+    console.log([++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
+    i++;
+}
+while (i < 100);  
+
