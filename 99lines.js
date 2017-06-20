9618493b2c0421189783541648e50b5a0f6874e0
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-20 09:02:59 -0500

    still not working

--- 99lines.js
+++ 99lines.js
@@ -1,8 +1,34 @@
-
-
-
-
 var friends = ['Bean ', 'Pierre ', 'Miesha ', 'Brandon ', 'Rayshawn '];
+
+// var button = documentCreateElement('button');
+// document.body.appendChild(button);
+// button.addEventListener('click',singSong)
+function singSong(){
+
+for (x=0;x<friends.length; x++){
+
+     var div = documentCreateElement('div');
+
+    div.className = 'div1';
+
+    var h3 = documentCreateElement('h3');
+
+    div1.appendChild(h3);
+
+    document.body.appendChild(div1);
+    h3.innerHTML = div1[i];
+    var para = document.createElement('p');
+    div.appendChild(para);
+
+    for (i=99; i > 0; i--) {
+
+       para.innerHTML = [++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall';
+    
+    }
+    para.innerHTML = [++i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' + ' no more lines of code on the wall';
+}
+}
+
 // for (x=0;x<friends.length; x++){
 //     for (i=99; i > 0; i--) {
 
@@ -11,26 +37,3 @@ var friends = ['Bean ', 'Pierre ', 'Miesha ', 'Brandon ', 'Rayshawn '];
 //     }
 //     console.log([++i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' + ' no more lines of code on the wall');
 // }
-
-
-// var button = documentCreateElement('button');
-// document.body.appendChild(button);
-// button.addEventListener('click',singSong)
-function singSong(){
-for (x=0;x<friends.length; x++){
-     var div = documentCreateElement('div');
-    div.className = 'div1';
-    var h3 = documentCreateElement('h3');
-    div1.appendChild(h3);
-    document.body.appendChild(div1);
-    for (i=99; i > 0; i--) {
-
-        console.log([++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
-    
-    }
-    console.log([++i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' + ' no more lines of code on the wall');
-}
-}
-
-
-
